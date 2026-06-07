de9652ee87bf4cfbb4e7ac34d3aa8e9563f1c547
aeltorgoman <abdelaziz.moh84@gmail.com>
2026-06-07 23:15:10 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ConfigProvider } from "./config/ConfigContext";
 import App from "./App";
 import "./index.css";
+import "./styles/themes.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
