6dc115bd32d2fce33e66d88f110ee9d581c3febc
aeltorgoman <abdelaziz.moh84@gmail.com>
2026-06-07 23:18:46 +0300

    Update ThemeContext.tsx

--- src/config/ThemeContext.tsx
+++ src/config/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState, ReactNode } from "react";
+import { createContext, useContext, useEffect, useState } from "react"; import type { ReactNode } from "react";
 import type { AppConfig } from "./AppConfig";
 
 // ===== أنواع الثيمات =====
