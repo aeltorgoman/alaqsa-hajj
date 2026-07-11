49627705e191af23363c74717ff3f5a4a8709b0a
aeltorgoman <abdelaziz.moh84@gmail.com>
2026-07-11 11:30:53 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ConfigProvider } from "./config/ConfigContext";
 import App from "./App";
+import { PilgrimPortal } from "./components/PilgrimPortal";
 import "./index.css";
 import "./styles/themes.css";
 
+const isPortal = window.location.pathname.startsWith("/hajj");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider>
-      <App />
-    </ConfigProvider>
+    {isPortal ? (
+      <PilgrimPortal />
+    ) : (
+      <ConfigProvider>
+        <App />
+      </ConfigProvider>
+    )}
   </StrictMode>
 );
